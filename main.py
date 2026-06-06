855c058dc8cb33aeef0ee6c1525a52a7f4dfa8e4
anasty11 <nastenka.makarova.2008@internet.ru>
2026-06-06 10:45:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print ('Hello World')
+if __name__ == "__main__":
+import tkinter as tk
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Мое приложение")
+        self.root.geometry("400x300")
+        
+        label = tk.Label(root, text="Привет, мир!")
+        label.pack()
+
+root = tk.Tk()
+App(root)
+root.mainloop()
